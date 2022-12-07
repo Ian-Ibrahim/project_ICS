b6dcdb3fe978048435967dee7702a0110e1a0853
Ian-Ibrahim <ian.okova@strathmore.edu>
2022-12-07 18:17:53 +0300

    front end

--- Anna/anna_frontend/hello.py
+++ Anna/anna_frontend/hello.py
@@ -1,24 +1,63 @@
 from flask import Flask, request, render_template
+import numpy as np
+import re
+import sys 
+import os
+import base64
+sys.path.append(os.path.abspath("../"))
 
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 app = Flask(__name__)
-
+def ValuePredictor(to_predict_list):
+    to_predict = np.array(to_predict_list).reshape(1, 1)
+    loaded_model = pickle.load(open("../models/model.pkl", "rb"))
+    result = loaded_model.predict(to_predict)
+    return result[0]
+ 
 @app.route("/")
 def home():
     return render_template('index.html')
 @app.route("/predict",methods=['GET','POST'])
 def predict():
-    furnished=0;
+    result=0;
+    month=5
+    
     if request.method =="POST":
-        houseType=request.form.get("type")
-        houseSubType=request.form.get("sub_type")
-        county=request.form.get("county")
-        locality=request.form.get("locality")
-        bedrooms=request.form.get("bedrooms")
-        bathrooms=request.form.get("bathrooms")
-        toilets=request.form.get("toilets")
-        parking=request.form.get("toilets")
-        furnished=request.form.get("furnished")
-    return render_template('predict.html',prediction=furnished)
+        to_predict_list = request.form.to_dict()
+        to_predict_list = list(to_predict_list.values())
+        to_predict_list = list(map(int, to_predict_list))
+        result = ValuePredictor(to_predict_list)  
+        # houseType=request.form.get("type")
+        # print(type(houseType))
+        # houseType=StandardScaler().fit_transform(float(houseType).reshape(-1, 1))
+        # houseSubType=request.form.get("sub_type")
+        # county=request.form.get("county")
+        # locality=request.form.get("locality")
+        # bedrooms=request.form.get("bedrooms")
+        # bathrooms=request.form.get("bathrooms")
+        # toilets=request.form.get("toilets")
+        # parking=request.form.get("toilets")
+        # furnished=request.form.get("furnished")
+        # serviced=request.form.get("serviced")
+        # shared=request.form.get("shared")
+        # month=request.form.get("month")
+        # year=request.form.get("year")
+        # if furnished is None:
+        #     furnished= int(0)
+        # if furnished=="on":
+        #     furnished=int(1)
+        # if serviced is None:
+        #     serviced= int(0)
+        # if serviced=="on":
+        #     serviced=int(1)
+        # if shared is None:
+        #     shared= int(0)
+        # if shared=="on":
+        #     shared=int(1)
+        #     variables=[houseType,houseSubType,county,locality,bedrooms,bathrooms,toilets,
+        # parking,furnished,serviced,shared,month,year]
+        # houseType=StandardScaler().fit_transform(houseType.reshape(-1, 1))
+    return render_template('predict.html',prediction=result)
 
 @app.route("/about")
 def about():
--- Anna/anna_frontend/static/js/loader.js
+++ Anna/anna_frontend/static/js/loader.js
@@ -15,19 +15,19 @@ function stateChange() {
   var localityOptions=localitySelect.children
   const state=document.getElementById('county').value
   for (let name of localityOptions) {
-    name.style.display="none" // main, and then page
+    name.style.display="none" 
   }
   if (state==1){
     const collection = document.getElementsByClassName("nairobi-option");
     for (let name of collection) {
-      name.style.display="block" // main, and then page
+      name.style.display="block" 
     }
     localitySelect.value=collection[0].value
   }
   if (state==2){
     const collection = document.getElementsByClassName("kiambu-option");
     for (let name of collection) {
-      name.style.display="block" // main, and then page
+      name.style.display="block"
     }
     localitySelect.value=collection[0].value
   }
@@ -46,4 +46,58 @@ function stateChange() {
     localitySelect.value=collection[0].value
   }
   
+}
+function typeChange() {
+  // for(var i=0; i<localityOptions.length; i++){
+  //   var localityChild = localityOptions[i];
+  //   localityOptions.style.color = "red";
+  // }
+  const subTypeSelect=document.getElementById("sub_type")
+  var subTypeOptions=subTypeSelect.children
+  const houseType=document.getElementById('type').value
+  for (let name of subTypeOptions) {
+    name.style.display="none" 
+  }
+  if (houseType==1){
+    const collection = document.getElementsByClassName("house-option");
+    for (let name of collection) {
+      name.style.display="block"
+    }
+    subTypeSelect.value=collection[0].value
+  }  
+  if (houseType==2){
+    const collection = document.getElementsByClassName("apartment-option");
+    for (let name of collection) {
+      name.style.display="block" 
+    }
+    subTypeSelect.value=collection[0].value
+  }
+}
+function setDate(){
+  document.getElementById("year").value=new Date().getFullYear()
+  document.getElementById("month").value= new Date().getMonth()
+}
+function sharedChange(){
+  shared=document.getElementById("shared")
+  if(shared.checked){
+    document.getElementById("sharedValue").value=1
+  }else{
+    document.getElementById("sharedValue").value=0
+  }
+}
+function furnishedChange(){
+  shared=document.getElementById("furnished")
+  if(shared.checked){
+    document.getElementById("furnishedValue").value=1
+  }else{
+    document.getElementById("furnishedValue").value=0
+  }
+}
+function servicedChange(){
+  shared=document.getElementById("serviced")
+  if(shared.checked){
+    document.getElementById("servicedValue").value=1
+  }else{
+    document.getElementById("servicedValue").value=0
+  }
 }
